7a43065b40e4370210392c0ebe911b1dd6106c12
ma-singh <masingh@fullsail.edu>
2017-04-05 18:12:49 -0400

    add container

---
+++ lecture/src/containers/ClubsList.js
@@ -0,0 +1,33 @@
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import {
+	orderByName,
+	orderByStanding,
+	selectClub,
+	showClubs
+} from '../core-module/actions'
+
+// import Clubs from '../components/Clubs'
+
+// mapStateToProps
+const mapStateToProps = (state) => {
+	let clubs = state.clubs.clubs
+
+	return {
+		clubs,
+		showClubs: state.club.showClubs
+		club: state.clubs.club
+	}
+}
+
+// mapDispatchToProps
+const mapDispatchToProps = (dispatch) => {
+	return bindActionCreators({
+		orderByName,
+		orderByStanding,
+		selectClub,
+		showClubs
+	}, dispatch)
+}
+
+export default connect(maptStateToProps, mapDispatchToProps)(Clubs)
--- lecture/src/index.js
+++ lecture/src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+import clubsReducer from './core-module/reducers'
+
+//import App from './App';
+//import './index.css';
+
+let store = createStore{clubsReducer}
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
 );
